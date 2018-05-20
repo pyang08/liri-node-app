d8f522b64511f00ebf759dea9568a75fb38edcea
pyang08 <pamelapyang@hotmail.com>
2018-05-19 22:10:07 -0500

    Update LIRI.js

--- LIRI.js
+++ LIRI.js
@@ -39,123 +39,120 @@ function getTweets() {
 
     // using the npm
     client.get('statuses/user_timeline', params, function (error, tweets) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            // for loop to run through the length of my account's tweets
-            console.log("\n-------------TWEET ME-------------\n");
-            for (i = 0; i < tweets.length; i++) {
-                // adds number and dot before to show order
-                console.log((i + 1) + ". " + tweets[i].text);
-            }
+        errorFunction();
+        // for loop to run through the length of my account's tweets
+        console.log("\n-------------TWEET ME-------------\n");
+        for (i = 0; i < tweets.length; i++) {
+            // adds number and dot before to show order
+            console.log((i + 1) + ". " + tweets[i].text);
         }
     });
-}
 
-// ------------------------ OMDB MOVIES---------------------- //
 
-function searchMovie(searchValue) {
+    // ------------------------ OMDB MOVIES---------------------- //
 
-    // Default search value if no movie is given
-    if (searchValue == "") {
-        searchValue = "Mr. Nobody";
-    }
+    function searchMovie(searchValue) {
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + searchValue.trim() + "&y=&plot=short&apikey=trilogy";
+        // Default search value if no movie is given
+        if (searchValue == "") {
+            searchValue = "Mr. Nobody";
+        }
 
-    request(queryUrl, function (respError, response, body) {
+        var queryUrl = "http://www.omdbapi.com/?t=" + searchValue.trim() + "&y=&plot=short&apikey=trilogy";
 
-        errorFunction();
+        request(queryUrl, function (respError, response, body) {
 
-        if (JSON.parse(body).Error == 'Movie not found!') {
+            errorFunction();
 
-            console.log("\nI'm sorry, we could not find " + searchValue + "! Please try again.\n")
+            if (JSON.parse(body).Error == 'Movie not found!') {
 
-            fs.appendFile("log.txt", "I'm sorry, we could not find " + searchValue + "! Please try again.\n\n-----OMDB Log Entry End-----\n\n", errorFunctionEnd());
+                console.log("\nI'm sorry, we could not find " + searchValue + "! Please try again.\n")
 
-        } else {
-
-            movieBody = JSON.parse(body);
-
-            console.log("\n--------------- OMDB Search Results ---------------\n");
-            console.log("Movie Title: " + movieBody.Title);
-            console.log("Year: " + movieBody.Year);
-            console.log("IMDB rating: " + movieBody.imdbRating);
-
-            // If there's no rating
-            if (movieBody.Ratings.length < 2) {
-
-                console.log("There is no Rotten Tomatoes Rating for this movie.")
-
-                fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: There is no Rotten Tomatoes Rating for this movie \nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+                fs.appendFile("log.txt", "I'm sorry, we could not find " + searchValue + "! Please try again.\n\n-----OMDB Log Entry End-----\n\n", errorFunctionEnd());
 
             } else {
 
-                console.log("Rotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value);
+                movieBody = JSON.parse(body);
 
-                fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value + "\nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+                console.log("\n--------------- OMDB Search Results ---------------\n");
+                console.log("Movie Title: " + movieBody.Title);
+                console.log("Year: " + movieBody.Year);
+                console.log("IMDB rating: " + movieBody.imdbRating);
+
+                // If there's no rating
+                if (movieBody.Ratings.length < 2) {
+
+                    console.log("There is no Rotten Tomatoes Rating for this movie.")
+
+                    fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: There is no Rotten Tomatoes Rating for this movie \nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+
+                } else {
+
+                    console.log("Rotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value);
+
+                    fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value + "\nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunction());
+                }
+
+                console.log("Country: " + movieBody.Country);
+                console.log("Language: " + movieBody.Language);
+                console.log("Plot: " + movieBody.Plot);
+                console.log("Actors: " + movieBody.Actors);
+                console.log("\n------------------------------------------------\n");
+            };
+        });
+    };
+
+
+    // ------------------------ SPOTIFY ---------------------- //
+
+    function searchSong() {
+
+        var spotify = new Spotify(keys.spotify);
+
+        spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
             }
 
-            console.log("Country: " + movieBody.Country);
-            console.log("Language: " + movieBody.Language);
-            console.log("Plot: " + movieBody.Plot);
-            console.log("Actors: " + movieBody.Actors);
-            console.log("\n------------------------------------------------\n");
-        };
-    });
-};
-
-
-// ------------------------ SPOTIFY ---------------------- //
-
-function searchSong() {
-
-    var spotify = new Spotify(keys.spotify);
-       
-      spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
+            console.log(data);
+        });
     }
 
-// ------------------------ DO-WHAT-IT-SAYS ---------------------- //
-function randomSearch() {
+    // ------------------------ DO-WHAT-IT-SAYS ---------------------- //
+    function randomSearch() {
 
-    fs.readFile("random.txt", "utf8", function (respError, data) {
+        fs.readFile("random.txt", "utf8", function (respError, data) {
 
-        var randomArray = data.split(", ");
+            var randomArray = data.split(", ");
 
-        errorFunction();
+            errorFunction();
 
-        if (randomArray[0] == "spotify-this-song") {
-            searchSong(randomArray[1]);
-        } else if (randomArray[0] == "movie-this") {
-            searchMovie(randomArray[1]);
-        } else {
+            if (randomArray[0] == "spotify-this-song") {
+                searchSong(randomArray[1]);
+            } else if (randomArray[0] == "movie-this") {
+                searchMovie(randomArray[1]);
+            } else {
+                getTweets();
+            }
+        });
+    };
+
+    // ------------------------ MAIN SWITCH CASE ---------------------- //
+
+    switch (command) {
+        case "my-tweets":
             getTweets();
-        }
-    });
-};
-
-// ------------------------ MAIN SWITCH CASE ---------------------- //
-
-switch (command) {
-    case "my-tweets":
-        getTweets();
-        break;
-    case "spotify-this-song":
-        searchSong(searchValue);
-        break;
-    case "movie-this":
-        searchMovie(searchValue);
-        break;
-    case "do-what-it-says":
-        randomSearch();
-        break;
-    default:
-        console.log("\nI'm sorry, " + command + " is not working. Please try one of the following commands: \n\n  1. To search a movie: node liri.js movie-this (with desired movie title following) \n\n  2. To search Spotify for a song: node liri.js spotify-this-song (with desired song title following) \n\n  3. To see my last 20 tweets from Twitter: node liri.js my-tweets \n");
-};
+            break;
+        case "spotify-this-song":
+            searchSong(searchValue);
+            break;
+        case "movie-this":
+            searchMovie(searchValue);
+            break;
+        case "do-what-it-says":
+            randomSearch();
+            break;
+        default:
+            console.log("\nI'm sorry, " + command + " is not working. Please try one of the following commands: \n\n  1. To search a movie: node liri.js movie-this (with desired movie title following) \n\n  2. To search Spotify for a song: node liri.js spotify-this-song (with desired song title following) \n\n  3. To see my last 20 tweets from Twitter: node liri.js my-tweets \n");
+    };
+}
